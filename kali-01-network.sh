7b828f9a2aaee0606d82f003821b78359106d3ef
xp.li <lixingping233@gmail.com>
2017-07-28 11:22:04 +0800

    Update kali-01-network.sh

--- kali-01-network.sh
+++ kali-01-network.sh
@@ -1,4 +1,4 @@
-
+echo "Configure network."
 echo -e "#The primary network interface \n \
 		auto eth0 \n \
 		iface eth0 inet dhcp \n" > /etc/network/interfaces.d/if-eth0
@@ -11,3 +11,4 @@ echo -e "#The secondary network interface \n \
 		network 192.168.1.0 \n \
 		broadcast 192.168.1.255 \n \
 		gateway 192.168.1.1 \n" > /etc/network/interfaces.d/if-eth1
+echo "Configure finish ."
