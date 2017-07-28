e44843e8c6b3d6c8cf409d215caef3a917424673
xp.li <lixingping233@gmail.com>
2017-07-28 11:16:19 +0800

    Create kali-01-network.sh

---
+++ kali-01-network.sh
@@ -0,0 +1,13 @@
+
+echo -e "#The primary network interface \n \
+		auto eth0 \n \
+		iface eth0 inet dhcp \n" > /etc/network/interfaces.d/if-eth0
+
+echo -e "#The secondary network interface \n \
+		auto eth1 \n \
+		iface eth1 inet static \n \
+		address 192.168.1.101 \n \
+		netmask 255.255.255.0 \n \
+		network 192.168.1.0 \n \
+		broadcast 192.168.1.255 \n \
+		gateway 192.168.1.1 \n" > /etc/network/interfaces.d/if-eth1
