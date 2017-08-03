72c7c70db09f03d0c2a1c54f3a203ee7cb6a2138
Lxp <lixingping233@163.com>
2017-08-03 17:42:28 +0800

    New docker script.

---
+++ docker/docker_certs.sh
@@ -0,0 +1,21 @@
+export HOST=192.168.1.122
+export HOST_IP=192.168.1.122
+rm -rf ~/cert
+mkdir ~/cert && cd ~/cert
+openssl genrsa -aes256 -out ca-key.pem 4096
+openssl req -new -x509 -days 365 -key ca-key.pem -sha256 -out ca.pem
+openssl genrsa -out server-key.pem 4096
+openssl req -subj "/CN=$HOST" -sha256 -new -key server-key.pem -out server.csr
+echo subjectAltName = DNS:$HOST,IP:$HOST_IP,IP:127.0.0.1 > extfile.cnf
+openssl x509 -req -days 365 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem \
+	-CAcreateserial -out server-cert.pem -extfile extfile.cnf
+openssl genrsa -out key.pem 4096
+echo extendedKeyUsage = clientAuth > extfile.cnf
+openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem \
+    -CAcreateserial -out cert.pem -extfile extfile.cnf
+rm -v client.csr server.csr
+chmod -v 0400 ca-key.pem key.pem server-key.pem
+chmod -v 0444 ca.pem server-cert.pem cert.pem
+mkdir -pv ~/.docker
+cp -v {ca,cert,key}.pem ~/.docker
+cp -r ~/cert/* /etc/docker/certs.d
